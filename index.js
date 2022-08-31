5ae8f110a0ad7cbcfaa587e9d87465c7194eaf83
zetavg <mail@zeta.vg>
2022-08-31 23:38:03 +0800

    add more ignored logs

--- index.js
+++ index.js
@@ -16,9 +16,14 @@ AppRegistry.registerComponent(appName, () => App);
 // AppRegistry.registerComponent(appName, () => StorybookUIRoot);
 
 LogBox.ignoreLogs([
+  'Unhandled promise rejection RangeError: Maximum call stack size exceeded (native stack depth)',
+  "AsyncStorage has been extracted from react-native core and will be removed in a future release. It can now be installed and imported from '@react-native-async-storage/async-storage' instead of 'react-native'. See https://github.com/react-native-async-storage/async-storage",
   'Could not find Fiber with id',
   'Non-serializable values were found in the navigation state.',
   'Require cycle: node_modules/react-native-paper/src/components/Appbar',
   'Require cycle: node_modules/pouchdb-utils/lib/index-browser.js',
   'Require cycle: node_modules/@craftzdog/pouchdb-adapter-websql-core',
+  'Require cycle: node_modules/pouchdb-find/node_modules/pouchdb-utils/lib/index-browser.js',
+  'Require cycle: node_modules/pouchdb-selector-core/node_modules/pouchdb-utils/lib/index-browser.js',
+  'node_modules/pouchdb-abstract-mapreduce/node_modules/pouchdb-utils/lib/index-browser.js',
 ]);
