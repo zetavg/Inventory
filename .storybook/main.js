fdc9b248b9b19155ecdfa0c8ea1df0a944bddc48
zetavg <mail@zeta.vg>
2023-06-25 19:19:21 +0800

    correct way to disable @storybook/addon-ondevice-actions

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,7 @@ module.exports = {
     '@storybook/addon-ondevice-controls',
     // TypeError: api.store().getStoryAndParameters is not a function. (In 'api.store().getStoryAndParameters(this.state.selection.kind, this.state.selection.story)', 'api.store().getStoryAndParameters' is undefined) (node_modules/@storybook/addon-ondevice-backgrounds/dist/BackgroundPanel.js:62:21)
     // '@storybook/addon-ondevice-backgrounds',
-    '@storybook/addon-ondevice-actions',
+    // Will cause Possible Unhandled Promise Rejection - RangeError: Maximum call stack size exceeded
+    // '@storybook/addon-ondevice-actions',
   ],
 };
--- .storybook/storybook.requires.js
+++ .storybook/storybook.requires.js
@@ -8,10 +8,6 @@ import {
 } from "@storybook/react-native";
 
 import "@storybook/addon-ondevice-controls/register";
-// Will cause Possible Unhandled Promise Rejection - RangeError: Maximum call stack size exceeded
-// import "@storybook/addon-ondevice-actions/register";
-
-import { argsEnhancers } from "@storybook/addon-actions/dist/modern/preset/addArgs";
 
 import { decorators, parameters } from "./preview";
 
@@ -22,11 +18,6 @@ if (decorators) {
 if (parameters) {
   addParameters(parameters);
 }
-
-// temporary fix for https://github.com/storybookjs/react-native/issues/327 whilst the issue is investigated
-try {
-  argsEnhancers.forEach((enhancer) => addArgsEnhancer(enhancer));
-} catch {}
 
 const getStories = () => {
   return [
