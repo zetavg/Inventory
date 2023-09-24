44f63654ad106d916ad317c4de4e73b23fe88885
zetavg <mail@zeta.vg>
2023-09-25 03:22:46 +0800

    ColorSelect: allow press and slide to select color

--- App/app/components/ColorSelect.tsx
+++ App/app/components/ColorSelect.tsx
@@ -1,5 +1,13 @@
-import React from 'react';
-import { StyleSheet, TouchableWithoutFeedback, View } from 'react-native';
+import React, { useCallback, useRef, useState } from 'react';
+import {
+  LayoutChangeEvent,
+  LayoutRectangle,
+  PanResponder,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
+import ReactNativeHapticFeedback from 'react-native-haptic-feedback';
 
 import titleCase from '@app/utils/titleCase';
 
@@ -28,10 +36,91 @@ type Props = {
 export default function ColorSelect({ value, onChange }: Props) {
   const colors = useColors();
 
+  const containerRef = useRef<View | null>(null);
+  const containerPageX = useRef<number | null>(null);
+  const colorItemLayouts = useRef<{
+    [key: string]: LayoutRectangle;
+  }>({});
+
+  const handleContainerLayout = () => {
+    containerRef.current?.measure(
+      (_x, _y, _width, _height, pageX) => (containerPageX.current = pageX),
+    );
+  };
+
+  const handleColorLayout = (
+    color: ColorSelectColor,
+    event: LayoutChangeEvent,
+  ) => {
+    const { x, y, width, height } = event.nativeEvent.layout;
+    colorItemLayouts.current[color] = { x, y, width, height };
+  };
+
+  const isPanHandled = useRef(false);
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onMoveShouldSetPanResponder: (_evt, gestureState) => {
+        const { dx, dy } = gestureState;
+        return isPanHandled.current || Math.abs(dx) > Math.abs(dy);
+      },
+      onPanResponderMove: (_evt, gestureState) => {
+        if (!isPanHandled.current && Math.abs(gestureState.dx) < 24) {
+          return;
+        }
+        isPanHandled.current = true;
+
+        if (typeof containerPageX.current !== 'number') return;
+        const containerX = containerPageX.current;
+
+        const { moveX } = gestureState;
+        const fingerX = moveX - containerX;
+
+        const hoveredColor = COLOR_SELECTOR_COLORS.find(color => {
+          const layout = colorItemLayouts.current[color];
+          if (!layout) return false;
+
+          const { x, width } = layout;
+          return fingerX >= x && fingerX <= x + width;
+        });
+
+        if (hoveredColor) changeColor(hoveredColor);
+      },
+      onPanResponderRelease: () => {
+        isPanHandled.current = false;
+      },
+      onPanResponderTerminate: () => {
+        isPanHandled.current = false;
+      },
+    }),
+  ).current;
+
+  const currentValueRef = useRef(value);
+  currentValueRef.current = value;
+  const changeColor = useCallback(
+    (color: ColorSelectColor) => {
+      if (currentValueRef.current !== color) {
+        ReactNativeHapticFeedback.trigger('clockTick');
+        currentValueRef.current = color;
+      }
+      onChange(color);
+    },
+    [onChange],
+  );
+
   return (
-    <View style={styles.container}>
+    <View
+      style={styles.container}
+      ref={containerRef}
+      onLayout={handleContainerLayout}
+      {...panResponder.panHandlers}
+    >
       {COLOR_SELECTOR_COLORS.map(color => (
-        <TouchableWithoutFeedback key={color} onPress={() => onChange(color)}>
+        <TouchableWithoutFeedback
+          key={color}
+          onPress={() => changeColor(color)}
+          onLayout={event => handleColorLayout(color, event)}
+        >
           <View
             style={[
               styles.colorItemContainer,
