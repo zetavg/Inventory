015bc0715893fdffb4639b0e8b8a731a28dfb6cd
zetavg <mail@zeta.vg>
2023-11-20 08:39:04 +0800

    fix tests

--- Data/lib/utils/__tests__/hasChanges.test.ts
+++ Data/lib/utils/__tests__/hasChanges.test.ts
@@ -1,24 +1,30 @@
 import hasChanges from '../hasChanges';
 
 it('works', () => {
-  expect(hasChanges({}, {})).toBe(false);
+  expect(hasChanges({}, {})).toBeFalsy();
 
-  expect(hasChanges({ a: 1 }, { b: 2 })).toBe(true);
-  expect(hasChanges({ a: 1 }, { a: 2 })).toBe(true);
-  expect(hasChanges({ a: 1 }, { a: '1' })).toBe(true);
-  expect(hasChanges({ a: 1 }, { a: undefined })).toBe(true);
-  expect(hasChanges({ a: 1 }, { a: 1, b: 2 })).toBe(true);
-  expect(hasChanges({ a: 1, b: 2 }, { a: 1 })).toBe(true);
+  expect(hasChanges({ a: 1 }, { b: 2 })).toBeTruthy();
+  expect(hasChanges({ a: 1 }, { a: 2 })).toBeTruthy();
+  expect(hasChanges({ a: 1 }, { a: '1' })).toBeTruthy();
+  expect(hasChanges({ a: 1 }, { a: undefined })).toBeTruthy();
+  expect(hasChanges({ a: 1 }, { a: 1, b: 2 })).toBeTruthy();
+  expect(hasChanges({ a: 1, b: 2 }, { a: 1 })).toBeTruthy();
 
-  expect(hasChanges({ a: 'hi' }, { a: 'hi' })).toBe(false);
-  expect(hasChanges({ a: 'hi' }, { a: 'hello' })).toBe(true);
+  expect(hasChanges({ a: 'hi' }, { a: 'hi' })).toBeFalsy();
+  expect(hasChanges({ a: 'hi' }, { a: 'hello' })).toBeTruthy();
 
-  expect(hasChanges({ a: [] }, { a: [] })).toBe(false);
-  expect(hasChanges({ a: [] }, { a: [1] })).toBe(true);
-  expect(hasChanges({ a: [1] }, { a: [] })).toBe(true);
+  expect(hasChanges({ a: [] }, { a: [] })).toBeFalsy();
+  expect(hasChanges({ a: [] }, { a: [1] })).toBeTruthy();
+  expect(hasChanges({ a: [1] }, { a: [] })).toBeTruthy();
 
-  expect(hasChanges({ a: {} }, { a: {} })).toBe(false);
-  expect(hasChanges({ a: { a: {} } }, { a: { a: {} } })).toBe(false);
-  expect(hasChanges({ a: { a: {} } }, { a: { a: [] } })).toBe(true);
-  expect(hasChanges({ a: { a: {} } }, { a: { a: { a: null } } })).toBe(true);
+  expect(hasChanges({ a: {} }, { a: {} })).toBeFalsy();
+  expect(hasChanges({ a: { a: {} } }, { a: { a: {} } })).toBeFalsy();
+  expect(hasChanges({ a: { a: {} } }, { a: { a: [] } })).toBeTruthy();
+  expect(hasChanges({ a: { a: {} } }, { a: { a: { a: null } } })).toBeTruthy();
+});
+
+it('returns change level', () => {
+  expect(hasChanges({ a: 1 }, { b: 2 })).toBeGreaterThan(10);
+  expect(hasChanges({ integrations: 1 }, { integrations: 2 })).toBeTruthy();
+  expect(hasChanges({ integrations: 1 }, { integrations: 2 })).toBeLessThan(10);
 });
