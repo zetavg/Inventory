888feadbfae29a12b537bb32dc204226e5d5c790
zetavg <mail@zeta.vg>
2024-01-18 03:10:52 +0800

    iOS BLE: also list paired and connected devices on BLE scan

--- App/ios/RFIDBlutoothManager.m
+++ App/ios/RFIDBlutoothManager.m
@@ -668,7 +668,25 @@
   [[NSRunLoop mainRunLoop] addTimer:_connectTime forMode:NSRunLoopCommonModes];
 //    }
     self.uuidDataList=[[NSMutableArray alloc]init];
+
+    // Create a CBUUID object with the desired UUID string
+    NSArray *serviceUUIDs = @[[CBUUID UUIDWithString:@"6E400001-B5A3-F393-E0A9-E50E24DCCA9E"]];
+
     [self.centralManager scanForPeripheralsWithServices:nil options:@{CBCentralManagerScanOptionAllowDuplicatesKey : @ YES}];
+
+    // Retrieve connected peripherals
+    NSArray *connectedPeripherals = [self.centralManager retrieveConnectedPeripheralsWithServices:serviceUUIDs];
+
+    // Iterate over the connected peripherals and report
+    for (CBPeripheral *peripheral in connectedPeripherals) {
+      BLEModel *model=[BLEModel new];
+      model.nameStr=peripheral.name;
+      model.rssStr=[NSString stringWithFormat:@"%d", 0];
+      model.addressStr=[peripheral.identifier UUIDString];
+      model.peripheral=peripheral;
+      [self.managerDelegate receiveDataWithBLEmodel:model result:@"0"];
+    }
+
 }
 - (void)connectTimeroutEvent
 {
