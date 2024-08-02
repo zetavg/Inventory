75809b7d8ca1767c89bfda1780bfce1c7aed5c1a
Pokai Chang <mail@zeta.vg>
2024-08-02 22:29:47 +0800

    try to fix

--- App/scripts/pod-install.sh
+++ App/scripts/pod-install.sh
@@ -4,4 +4,5 @@ cd "$(dirname "$0")"
 cd ..
 cd ios
 echo "Cocoapods version: $(pod --version)"
+pod update hermes-engine --no-repo-update # It seems like you've changed the version of the dependency `hermes-engine` and it differs from the version stored in `Pods/Local Podspecs`. You should run `pod update hermes-engine --no-repo-update` to apply changes made locally. - IDK why commiting the updated Podfile.lock doesn't work, so we're doing this here
 pod install
